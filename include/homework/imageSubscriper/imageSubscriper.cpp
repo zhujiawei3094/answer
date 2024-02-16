68a3fa54597e2f308ae83f9abd0878f47669479b
zhujiawei3094 <158048021+zhujiawei3094@users.noreply.github.com>
2024-02-16 22:08:26 +0800

    Update imageSubscriper.cpp
    ti gao zhi xian jian ce jing zhun du

--- include/homework/imageSubscriper/imageSubscriper.cpp
+++ include/homework/imageSubscriper/imageSubscriper.cpp
@@ -23,23 +23,16 @@ void imageSubscriber::callback(sensor_msgs::msg::Image msg){
 void imageSubscriber::image_deal() {
     cv::Mat image=cv::imread("output.png",0);
     cv::Mat dst,cdst;
-    std::vector<cv::Vec2f> lines;
+    std::vector<cv::Vec4i> lines;
     cv::Canny(image,dst,50,200,3);
     cv::cvtColor(dst,cdst,CV_GRAY2BGR);
-    cv::HoughLines(dst,lines,1,CV_PI/180,100,0,0);
+    cv::HoughLinesP(dst,lines,1,CV_PI/180,50,400,10);
     for(size_t i=0;i<lines.size();i++){
-        float rho=lines[i][0],theta=lines[i][0];
-        cv::Point pt1,pt2;
-        double a=cos(theta),b=sin(theta);
-        double x0=a*rho,y0=b*rho;
-        pt1.x=cvRound(x0+1000*(-b));
-        pt1.y=cvRound(y0+1000*(a));
-        pt2.x=cvRound(x0-1000*(-b));
-        pt2.y=cvRound(y0-1000*(a));
-        cv::line(cdst,pt1,pt2,cv::Scalar(0,0,255),3,0);
+        cv::Vec4i l=lines[i];
+        cv::line(cdst,cv::Point(l[0],l[1]),cv::Point (l[2],l[3]),cv::Scalar(0,0,255),3,cv::LINE_AA);
 
     }
-    cv::imshow("i",image);
+
     cv::imshow("p",dst);
     cv::imshow("j",cdst);
     cv::waitKey(100);
