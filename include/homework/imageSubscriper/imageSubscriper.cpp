a2fdc41ea800c8a3c67e8c5397c20e4ca1a39227
zhujiawei3094 <158048021+zhujiawei3094@users.noreply.github.com>
2024-02-19 19:01:38 +0800

    Update imageSubscriper.cpp

--- include/homework/imageSubscriper/imageSubscriper.cpp
+++ include/homework/imageSubscriper/imageSubscriper.cpp
@@ -7,12 +7,10 @@ void imageSubscriber::image_callback(sensor_msgs::msg::Image msg){
     cv_bridge::CvImagePtr cvImage;
     cvImage = cv_bridge::toCvCopy(msg,msg.encoding);
     cv::Mat image = cvImage->image;
-    std::string outputFilename = "output.png";
-    cv::imwrite(outputFilename, image);
-    //image_deal(image);
+    //std::string outputFilename = "output.png";
+   // cv::imwrite(outputFilename, image);
+    image_deal(image);
 
-    cv::imshow("output.png",image);
-    cv::waitKey(100);
 }
 void imageSubscriber::image_deal(cv::Mat image) {
     cv::Mat dst,cdst;
@@ -30,18 +28,20 @@ void imageSubscriber::image_deal(cv::Mat image) {
     float k=(judge_line[1]-judge_line[3])/(judge_line[0]-judge_line[2]);
     float divid= sqrt(k*k+1);
     b_line.push_back(judge_line[1]-k*judge_line[0]);
-    /*for(size_t i=0;i<lines.size();i++){
+    for(size_t i=0;i<lines.size();i++){
         cv::Vec4i l=lines[i];
         b_line.push_back(l[1]-k*l[0]);
-        cv::line(cdst,cv::Point(l[0],l[1]),cv::Point (l[2],l[3]),cv::Scalar(0,0,255),3,cv::LINE_AA);
-    }*/
+        //cv::line(cdst,cv::Point(l[0],l[1]),cv::Point (l[2],l[3]),cv::Scalar(0,0,255),3,cv::LINE_AA);
+    }
     for(size_t i=1;i<b_line.size();i++){
         int distance =(b_line[0]-b_line[i])/divid;
-        if(distance>10&&distance<=30)
+       // std::cout<<"b:"<<b_line[i]<<"di:"<<distance<<std::endl;
+        if(distance<=30)
         {
             geometry_msgs::msg::Point32 point;
             point.set__x((lines[i][0]+lines[i][2])/2);
             point.set__y((lines[i][1]+lines[i][3])/2);
+           // std::cout<<point.x<<"   "<<point.y<<std::endl;
             msg.push(point);
         }
     }
@@ -51,11 +51,10 @@ void imageSubscriber::image_deal(cv::Mat image) {
     cv::waitKey(100);*/
 }
 void imageSubscriber::click_callback() {
-    geometry_msgs::msg::Point32 p;
-    p.set__x(820);
-    p.set__y(742);
-    msg.push(p);
-    clickPoint->publish(msg.front());
-    msg.pop();
-    std::cout<<p.x<<std::endl;
+    if(!msg.empty()){
+        geometry_msgs::msg::Point32 p;
+        clickPoint->publish(msg.front());
+        msg.pop();
+        //std::cout<<p.x<<std::endl;
+    }
 }
